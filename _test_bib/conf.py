15fe5cb6bb07f0bc96ae9172b765ac248f002082
jin <k.lee-5@student.tudelft.nl>
2026-02-22 15:25:24 +0100

    Test complete

--- _test_bib/conf.py
+++ _test_bib/conf.py
@@ -6,10 +6,33 @@ extensions = [
 bibtex_bibfiles = ["existing.bib"]
 bibtex_default_style = "unsrt"
 
+project = "Full Test"
+author = "Config Author"
+copyright = "2024 Config Copyright"
+
 metadata_figure_settings = {
     "style": {
         "placement": "caption",
-        "show": "author,license,date",
+        "show": "author,license,date,copyright,source",
+    },
+    "license": {
+        "substitute_missing": False,
+        "individual": True,
+    },
+    "author": {
+        "substitute_missing": True,
+        "default_author": "config",
+    },
+    "date": {
+        "substitute_missing": True,
+        "default_date": "2025-01-01",
+    },
+    "copyright": {
+        "substitute_missing": True,
+        "default_copyright": "authoryear",
+    },
+    "source": {
+        "warn_missing": False,
     },
     "bib": {
         "extract_metadata": True,
@@ -18,6 +41,5 @@ metadata_figure_settings = {
     },
 }
 
-project = "BibTest"
 master_doc = "index"
 exclude_patterns = ["_build"]
